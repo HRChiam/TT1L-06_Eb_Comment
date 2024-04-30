3e4ff613da317f52237e79a9535dc8e4b5c25a8b
KEE HUI YEE <keehuiyee0924@gmail.com>
2024-04-30 20:38:36 +0800

    database1

--- Admin/app.py
+++ Admin/app.py
@@ -2,11 +2,17 @@ from flask import Flask,render_template,redirect
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
+
+@app.route("/user")
+def user():
+    return render_template("user.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+    
 
 
