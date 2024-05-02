873792f8becdcd6ec9cf7e32a531d44a2aa5402c
KEE HUI YEE <keehuiyee0924@gmail.com>
2024-05-02 23:21:33 +0800

    database_flask_environment

--- Admin/app.py
+++ Admin/app.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template,redirect
+from models import UserForm, Users,db
 
 app = Flask(__name__)
 
@@ -9,6 +10,22 @@ def home():
 @app.route("/user")
 def user():
     return render_template("user.html")
+
+# will be update
+@app.route('/user/add', methods=['GET','POST'])
+def add_user():
+    name = None
+    form = UserForm()
+    if form.validate_on_submit(): # No duplicate records
+        user = Users.query.filter_by(email= form.email.data).first()
+        if user is None:
+            user = Users(name = form.name.data, email = form.name.data)
+            db.session.add(user)
+            db.session.commit()
+        name = form.name.data
+        form.name.data = ''
+        form.email.data = ''
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True)
