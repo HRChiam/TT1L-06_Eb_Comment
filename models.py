9005d0867f1a09ed9bad1bd46dd4c7b85038a046
Chiam Huai Ren <chiamrh@gmail.com>
2024-05-14 10:15:07 +0800

    combining code

--- models.py
+++ models.py
@@ -2,14 +2,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class Users(db.Model):
+    name = db.Column(db.String(20), primary_key=True)
+    email = db.Column(db.String(200), nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
 class Lecturer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     photo = db.Column(db.String(100), nullable=False)
     phone = db.Column(db.String(20), nullable=False)
-    email = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(200), nullable=False)
     campus = db.Column(db.String(100), nullable=False)
     faculty = db.Column(db.String(100), nullable=False)
-
-    def __repr__(self):
-        return '<Lecturer %r>' % self.name
