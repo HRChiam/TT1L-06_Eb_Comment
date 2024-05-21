cb3d91c48115a92e611edf433cbd69fb832763f6
Leong Wing Yan <victorialeongg@gmail.com>
2024-05-21 22:11:46 +0800

    .

--- layout/app.py
+++ layout/app.py
@@ -1,18 +1,15 @@
-import os
-from flask import Flask, render_template, request, redirect, url_for, session
-# from flask_sqlalchemy import SQLAlchemy
-from models import db, Users, Lecturer, Faculty, LecturerTemp
+from datetime import datetime
+from flask import Flask, request, render_template , request, redirect, session, url_for
+from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Message ,Mail
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from itsdangerous import URLSafeTimedSerializer
 
-app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'your_secret_key'
-
+app = Flask(__name__, template_folder='templates')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SECRET_KEY'] = 'your_secret_key_here'
+
 app.config['MAIL_SERVER'] = 'smtp-mail.outlook.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
@@ -20,44 +17,18 @@ app.config['MAIL_USE_SSL']= False
 app.config['MAIL_USERNAME'] = 'ebcomment123@outlook.my'
 app.config['MAIL_PASSWORD'] = 'hjszkqeytfsdnldp'
 app.config['MAIL_DEFAULT_SENDER'] ='ebcomment123@outlook.my'
+
 app.secret_key = 'your_secret_key_here'
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SECURITY_PASSWORD_SALT'] = 'your_salt'
 
-
+db = SQLAlchemy(app)
 mail = Mail(app)
 serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
 
-
-
-
-
-
-
-
-
-db.init_app(app)
-
-@app.errorhandler(404)
-def page_not_found(error):
-    # Render the error.html template with a custom message
-    return render_template('error.html', message="Page not found"), 404
-
-
-@app.errorhandler(500)
-def internal_server_error(error):
-    # Render the error.html template with a custom message
-    return render_template('error.html', message="Internal server error"), 500
-
-
 @app.route('/')
 def home():
-    return render_template('home.html')
-
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
+    return render_template ('home.html')
 
 
 @app.route('/signin')
@@ -65,15 +36,29 @@ def signin():
     return render_template('signin.html')
 
 
+@app.route('/login')
+def login():
+    return render_template ('login.html')
+
+
 @app.route('/forgot')
 def forgot():
-    return render_template('forgot.html')
-
+    return render_template ('forgot.html')
 
 
 @app.route('/profile')
 def profile():
     return render_template ('profile.html')
+
+
+@app.route('/student')
+def student():
+    return render_template('student.html')
+
+
+@app.route('/lec')
+def lec():
+    return render_template('lec.html')
 
 
 @app.route('/reset_password_form')
@@ -86,44 +71,12 @@ def invalid():
     return render_template('invalid.html')
 
 
-@app.route('/process_signin', methods=['POST'])
-def process_signin():
-    if request.method == 'POST':
-        nickname = request.form['nickname']
-        email = request.form['email']
-        password = request.form['password']
-        confirm_password = request.form['confirm_password']
-
-        error= {}
-
-        if email.endswith('@student.mmu.edu.my') and len(email.split('@')[0]) == 10:
-            pass
-        elif email.endswith('@mmu.edu.my'):
-            pass
-        else:
-            error['email'] = "Email invalid or does not meet requirements"
-
-        if not (len(password) >= 8 and sum(c.isdigit() for c in password) >= 4) or password != confirm_password:
-            error['password'] = "Password does not match or does not meet requirements"
-
-        if not error:
-            new_user = Users(name=nickname, email=email, password=password)
-            db.session.add(new_user)
-            db.session.commit()
-
-            if email.endswith('@student.mmu.edu.my'):
-                return redirect('/front')
-            elif email.endswith('@mmu.edu.my'):
-                return redirect('/index')
-
-        return render_template('signin.html', error=error)
-
-    return render_template('signin.html')
 
 
 @app.route('/process_login', methods=['GET', 'POST'])
 def process_login():
     if request.method == 'POST':
+        # username = request.form['username']
         email = request.form['email']
         password = request.form['password']
 
@@ -131,11 +84,11 @@ def process_login():
         student_email = email.endswith('@student.mmu.edu.my')and len(email.split('@')[0]) == 10
 
         if student_email:
-            return redirect('/front') 
+            return redirect(url_for('student')) 
         
 
         elif email.endswith('@mmu.edu.my'):
-            return redirect('/index')
+            return redirect(url_for('lec'))
 
         else:
             error_message = "Email invalid or does not meet requirements"
@@ -145,67 +98,50 @@ def process_login():
     return render_template('login.html')
 
 
-@app.route('/front')
-def studentfront():
-    return render_template('studentfront.html')
 
-@app.route('/main')
-def studentmain():
-    return render_template('studentmain.html')
 
-@app.route('/faculty/<faculty_name>')
-def faculty_page(faculty_name):
-    # Query the database to retrieve the faculty information
-    faculty = Faculty.query.filter_by(name=faculty_name).first()
+@app.route('/process_signin', methods=['POST'])
+def process_signin():
+    if request.method == 'POST':
+        email = request.form['email']
+        nickname = request.form['nickname']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
 
-    if faculty:
-        # If the faculty is found, fetch information about the lecturers associated with that faculty
-        lecturers = faculty.lecturers
-        # Render the faculty page template with the fetched data
-        return render_template('faculty_page.html', faculty=faculty, lecturers=lecturers)
-    else:
-        # If the faculty is not found, render an error page
-        return render_template('error.html', message="Faculty not found")
+        error= {}
+       
+        
+
+        if email.endswith('@student.mmu.edu.my') and len(email.split('@')[0]) == 10:
+            pass
+
+        elif email.endswith('@mmu.edu.my'):
+            pass
+        else:
+
+            error['email'] = "Email invalid or does not meet requirements"
+
+
+
+
+        if not (len(password) >= 8 and sum(c.isdigit() for c in password) >= 4) or password != confirm_password:
+            error['password'] = "Password does not match or does not meet requirements"
+
+        
+        if not error and email.endswith('@student.mmu.edu.my'):
+            return redirect(url_for('student'))
+        
+
+        if not error and email.endswith('@mmu.edu.my'):
+            return redirect(url_for('lec'))
+
+
+        return render_template('signin.html', error=error)
+
     
-@app.route('/keyin')
-def keyin():
-    return render_template('keyin.html')
+    return render_template('signin.html')
 
-@app.route('/upload', methods=['POST'])
-def upload():
-    name = request.form['name']
-    photo = request.files['photo']
-    phone = request.form['phone']
-    email = request.form['email']
-    campus = request.form['campus']
-    faculty_id = request.form['faculty']
 
-    if not os.path.exists('uploads'):
-        os.makedirs('uploads')
-
-    photo.save(os.path.join('uploads/' + photo.filename))
-
-    lecturer_temp = LecturerTemp(name=name, photo=photo.filename, phone=phone, email=email, campus=campus, faculty_id=faculty_id)
-    db.session.add(lecturer_temp)
-    db.session.commit()
-
-    return redirect(url_for('keyinsuccess'))
-
-@app.route('/keyinsuccess')
-def keyinsuccess():
-    return render_template('keyinsuccess.html')
-
-@app.route("/index")
-def index():
-    return render_template("index.html")
-
-@app.route("/user")
-def user():
-    return render_template("user.html")
-
-@app.route("/comment")
-def comment():
-    return render_template("comment.html")
 
 
 @app.route('/logout', methods=['POST'])
@@ -291,7 +227,6 @@ def reset_form():
     return render_template('reset_password_form.html')
 
 
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all() 
     app.run(debug=True)
