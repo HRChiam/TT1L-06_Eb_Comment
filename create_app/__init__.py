97ac65684ca08bed2020c8bed1e92c73e1373530
Chiam Huai Ren <chiamrh@gmail.com>
2024-06-10 13:18:45 +0800

    edit

--- create_app/__init__.py
+++ create_app/__init__.py
@@ -5,7 +5,6 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('config.Config')  # Assuming you have a Config class in a config.py file
     db.init_app(app)
     
     return app
